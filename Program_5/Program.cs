c6742bcfd59a8756c1d481a906e9f18c6cf4256f
Darshil Panchani <132995923+darshil1711@users.noreply.github.com>
2023-06-14 19:08:09 +0530

    Add files via upload

---
+++ Program_5/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Program_5
+{
+    class ForLoop
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        /// for loop use.
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter Number:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            for (int p = 0; p <= number2; p++)
+            {
+                for (int q = 1; q <= number2 - p; q++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int r = 1; r <= 2 * p - 1; r++)
+                {
+                    Console.Write("* ");
+
+                }
+
+                Console.WriteLine();
+              
+            }
+        }
+    }
+}
---
+++ Program_6/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Program_6
+{
+    class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            BreakContinue breakContinue = new BreakContinue();
+
+        }
+    }
+}
