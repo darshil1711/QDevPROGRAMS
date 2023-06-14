004a2ca3991455b15252f50d50e703de9080d3cf
Darshil Panchani <132995923+darshil1711@users.noreply.github.com>
2023-06-14 19:17:34 +0530

    Add files via upload

---
+++ Program_21/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program_21
+{
+    public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            List<string> myList = new List<string> { "A", "B", "C", "D", "A", "C", "E", "E", "E" };
+            Console.WriteLine("Original List : ");
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            // remove duplicate values
+            List<string> uniqueList = new List<string>(new HashSet<string>(myList));
+            Console.WriteLine("\nList Without Duplicate :");
+
+            foreach (var item in uniqueList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program_22/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Program_22
+{
+    public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            string path = @"D:\myTestFile.txt";
+            try
+            {
+                using (FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (StreamWriter writer = new StreamWriter(fileStream))
+                    {
+                        writer.WriteLine("Name : Darshil Patel");
+                        writer.WriteLine("Age : 21 year");
+                        writer.WriteLine("Location : Ahmedabad");
+                        writer.WriteLine("Occupation : Software Devloper");
+                        Console.WriteLine("File Created and Data Add Successfully.");
+                    }
+                    using (StreamReader reader = File.OpenText(path))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found!");
+                Console.WriteLine("Exception : " + ex.Message);
+            }
+
+        }
+    }
+}
