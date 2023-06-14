440f53cb0c6bb4fea6aae4c2bde85d58e4b63a5b
Darshil Panchani <132995923+darshil1711@users.noreply.github.com>
2023-06-14 19:16:22 +0530

    Add files via upload

---
+++ Program_19/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Program_19
+{
+     public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        /// add string values.
+        /// values with comma separated.
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string>();
+            nameList.Add("Darshil");
+            nameList.Add("Patel");
+            nameList.Add("(DP)");
+
+            Console.WriteLine(string.Join(",",nameList));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program_20/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Program_20
+{
+    public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        /// add dictionary as string in key value pair.
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> details = new Dictionary<string, string>();
+            details.Add("Student Name", "Darshil Patel");
+            details.Add("Student Education", "BCA");
+            details.Add("Student Location", "Ahmedabad");
+
+            foreach (var pair in details)
+            {
+                string keyValueString = string.Format("{0} = {1}",pair.Key, pair.Value);
+                Console.WriteLine(keyValueString);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
