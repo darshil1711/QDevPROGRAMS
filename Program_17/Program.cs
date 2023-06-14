80901f27ae7b4702c93b22713b6251f30c37035b
Darshil Panchani <132995923+darshil1711@users.noreply.github.com>
2023-06-14 19:15:27 +0530

    Add files via upload

---
+++ Program_17/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Program_17
+{
+    public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <exception cref="System.IndexOutOfRangeException">Index is out of range.</exception>
+        public static void Main()
+        {
+            try
+            {
+                int[] numbers = { 1, 2, 3 };
+                int index = 2;
+
+                if (index >= numbers.Length)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+
+                int value = numbers[index];
+                Console.WriteLine("Value at index " + index + ": " + value);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("IndexOutOfRangeException caught: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
+            }
+
+            Console.WriteLine("Program execution continues...");
+        }
+    }
+}
---
+++ Program_18/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program_18
+{
+    public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine("Enter our List Element to Find the Number : ");
+            int elementToFind = Convert.ToInt32(Console.ReadLine());
+
+            //index of array element
+            int index = Array.IndexOf(numbers, elementToFind);
+
+            if (index != 1)
+            {
+                Console.WriteLine("Element {0} found at index {1}", elementToFind, index);
+            }
+            else
+            {
+                Console.WriteLine("Element {0} not found in the array ", elementToFind);
+            }
+            Console.ReadKey();
+        }
+    }
+}
